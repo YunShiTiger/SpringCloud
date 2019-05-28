88cb4a0e56078f959f929c5295e85d3b371c91eb
unknown <845407803@qq.com>
2019-05-28 23:01:36 +0800

    解析部分

--- mybatis-3.5.1/src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
+++ mybatis-3.5.1/src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
@@ -47,16 +47,32 @@ import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.type.JdbcType;
 
 /**
- * @author Clinton Begin
- * @author Kazuki Shimizu
+ * 不错的解析文章
+ *   https://blog.csdn.net/qq_35807136/article/details/79002470
+ *   系列文章
+ *   https://blog.csdn.net/u013510838/article/details/78995542
+ *   不错的文章
+ *   http://www.cnblogs.com/zhjh256/p/8512392.html
+ *
+ *   比较详细的介绍属性信息的文章
+ *   https://www.cnblogs.com/li3807/p/7061835.html
+ *
+ *  通过提供的总的xml文件来完成配置信息对象的处理  其实起这个名字就能看出  者也是一种构造器模式  最终要构造的对象时配置信息类对象  而配置的来源可以分为字符流和字节流等不同的xml文件来源
  */
 public class XMLConfigBuilder extends BaseBuilder {
 
+  //用于表示是否已经进行过解析处理   即对应的xml文件解析操作只能够解析一次   即通过提供的xml配置文件完成一次配置配置信息类对象的处理
   private boolean parsed;
+  //真正进行解析给定xml文件的xpath解析器对象
   private final XPathParser parser;
+  //用于记录提供的数据库环境表示
   private String environment;
+  //
   private final ReflectorFactory localReflectorFactory = new DefaultReflectorFactory();
 
+  /**
+   * 提供通过字符流方式的xml文件来构建对象的处理
+   */
   public XMLConfigBuilder(Reader reader) {
     this(reader, null, null);
   }
@@ -69,6 +85,9 @@ public class XMLConfigBuilder extends BaseBuilder {
     this(new XPathParser(reader, true, props, new XMLMapperEntityResolver()), environment, props);
   }
 
+  /**
+   * 提供通过字节流方式的xml文件来构建对象的处理
+   */
   public XMLConfigBuilder(InputStream inputStream) {
     this(inputStream, null, null);
   }
@@ -81,29 +100,71 @@ public class XMLConfigBuilder extends BaseBuilder {
     this(new XPathParser(inputStream, true, props, new XMLMapperEntityResolver()), environment, props);
   }
 
+  /**
+   * 最终需要走的构造函数   前面的构造函数主要完成了相关对象的转换操作处理  即xml文件解析器对象的构建
+   *  注意本方法是一个私有的方法  即只能通过上述的构造函数进行调用，不对外提供构建对象的操作处理
+   *    这样做的主要原因是 防止误操作对此创建配置信息类对象
+   */
   private XMLConfigBuilder(XPathParser parser, String environment, Properties props) {
+    //通过这个方法调用可以发现 在子类中创建了配置信息类对象.然后传递给父类中,最后通过父类对象中的引用在子类对象中进行使用
+    //这里这样做的原因可以是  统一在父类中使用这个配置对象  同时暴露只有一个地方来进行new对应对象的入口  从而保证配置对象的唯一性
+    //即其他子类对象调用此处的方式绝对不是通过new对象来创建此对象  只有此处是通过new对象来传入的  从而完成唯一性的保证
     super(new Configuration());
     ErrorContext.instance().resource("SQL Mapper Configuration");
+    //这个地方需要注意  这个地方首次进行了设置配置信息对象中属性列表中的值   即此处的值是通过最外边的门面类对象传入的
     this.configuration.setVariables(props);
+    //初始化当前尚未进行解析操作处理标识
     this.parsed = false;
+    //记录对应的数据库环境标识
     this.environment = environment;
+    //记录对应的xml解析对象
     this.parser = parser;
   }
 
+  /**
+   * 对外暴露的根据配置的构建器对象来解析配置的xml文件处理
+   *   此处完成对配置信息类对象的一次相关数据信息的初始化操作处理   即根据提供的配置xml来对配置信息类对象完成一些全局方面的配置处理
+   *
+   *   即配置信息类对象的配置数据来源主要来自两个方面  1 全局性配置的xml文件   2 mapper的xml文件
+   */
   public Configuration parse() {
+    //首先进行检测是否已经解析过对应配置文件的处理
     if (parsed) {
+      //已经解析过就不能进行解析处理了  因此这里抛出错误处理
       throw new BuilderException("Each XMLConfigBuilder can only be used once.");
     }
+    //设置解析操作位为已经进行过解析操作处理标识
     parsed = true;
+    //触发解析操作处理   内部首先找到对应的configuration节点  即根配置节点
     parseConfiguration(parser.evalNode("/configuration"));
+    //返回根据解析的配置文件构建的配置类对象
     return configuration;
   }
 
+  /**
+   * 分别进行解析对应节点的框架处理函数    传入的参数为对应的根节点
+   * 解析对应的节点参数  https://blog.csdn.net/lovebosom/article/details/52584562
+   * mybatis-Config.xml配置文件的内容和配置顺序如下
+      properties(属性)
+      settings(全局配置参数)
+      typeAiases(类型别名)
+      typeHandlers(类型处理器)
+      objectFactory(对象工厂)
+       plugins(插件)
+      environments(环境集合属性对象)mappers(映射器)
+      environment(环境子属性对象)
+      transactionManager(事物管理)
+      datesource(数据源)
+      mappers(映射器)
+   */
   private void parseConfiguration(XNode root) {
     try {
       //issue #117 read properties first
+      //加载properties节点,一般是定义一些配置变量
+      //解析元素properties，保存在variables中      如配置dataSource的username password
       propertiesElement(root.evalNode("properties"));
       Properties settings = settingsAsProperties(root.evalNode("settings"));
+
       loadCustomVfs(settings);
       loadCustomLogImpl(settings);
       typeAliasesElement(root.evalNode("typeAliases"));
@@ -111,6 +172,7 @@ public class XMLConfigBuilder extends BaseBuilder {
       objectFactoryElement(root.evalNode("objectFactory"));
       objectWrapperFactoryElement(root.evalNode("objectWrapperFactory"));
       reflectorFactoryElement(root.evalNode("reflectorFactory"));
+
       settingsElement(settings);
       // read it after objectFactory and objectWrapperFactory issue #631
       environmentsElement(root.evalNode("environments"));
@@ -118,19 +180,94 @@ public class XMLConfigBuilder extends BaseBuilder {
       typeHandlerElement(root.evalNode("typeHandlers"));
       mapperElement(root.evalNode("mappers"));
     } catch (Exception e) {
+      //在解析对应节点过程出现异常就抛出对应的错误异常  注意这个地方包含了root节点为空的异常  即配置根节点出现了问题
       throw new BuilderException("Error parsing SQL Mapper Configuration. Cause: " + e, e);
     }
   }
 
+  /** 加载properties节点,一般是定义一些配置变量   需要明确优先级问题
+   * 从这个方法中可以看出配置规则
+       可以设置url或resource属性从外部文件中加载一个properties文件
+       可以通过property子节点进行配置，如果子节点属性的key与外部文件的key重复的话，子节点同名属性值将被覆
+       暴露在最外边的门面传入的属性值具有最高优先级
+   properties配置示例
+     <properties resource="org/mybatis/example/config.properties">
+       <property name="username" value="dev_user"/>
+       <property name="password" value="F2Fa3!33TYyg"/>
+     </properties>
+
+   xxx.properties的配置示例：
+     jdbc.driver=com.mysql.jdbc.Driver
+     jdbc.url=jdbc:mysql://localhost:3306/mybatis
+     jdbc.username=root
+     jdbc.password=root
+   */
+  private void propertiesElement(XNode context) throws Exception {
+    //首先检测对应的节点是否存在
+    if (context != null) {
+      //先加载property子节点下的属性
+      Properties defaults = context.getChildrenAsProperties();
+      //获取在节点上的两个属性参数是否设置
+      String resource = context.getStringAttribute("resource");
+      String url = context.getStringAttribute("url");
+      //不能同时设置resource属性和url属性
+      if (resource != null && url != null) {
+        throw new BuilderException("The properties element cannot specify both a URL and a resource based property file reference.  Please specify one or the other.");
+      }
+      //此处说明了一处属性值的覆盖问题
+      if (resource != null) {
+        //加载对应的外部文件配置的属性值 并进行覆盖同名的属性值
+        defaults.putAll(Resources.getResourceAsProperties(resource));
+      } else if (url != null) {
+        //加载对应的外部文件配置的属性值 并进行覆盖同名的属性值
+        defaults.putAll(Resources.getUrlAsProperties(url));
+      }
+      Properties vars = configuration.getVariables();
+      //此处说明了一处属性值的覆盖问题
+      if (vars != null) {
+        //通过暴露在最外边的门面传入的属性值具有最高优先级  即可以覆盖同名的属性值
+        defaults.putAll(vars);
+      }
+      //将全部的属性参数设置到解析器上，方便解析器进行后续解析操作时进行使用
+      parser.setVariables(defaults);
+      //最终将对应的属性节点的信息设置到配置信息对象中  方便其他类对象进行使用
+      configuration.setVariables(defaults);
+    }
+  }
+
+  /**
+   * settings加载全局配置参数 同时完成对配置对象中属性是否存在对应的set方法的检测处理
+    官方给的settings配置实例
+   	<settings name="cacheEnabled" value="true"/>
+      <setting name="lazyLoadingEnabled" value="true"/>
+      <setting name="multipleResultSetsEnabled" value="true"/>
+      <setting name="useColumnLabel" value="true"/>
+      <setting name="useGeneratedKeys" value="false"/>
+      <setting name="autoMappingBehavior" value="PARTIAL"/>
+      <setting name="defaultExecutorType" value="SIMPLE"/>
+      <setting name="defaultStatementTimeout" value="25"/>
+      <setting name="safeRowBoundsEnabled" value="false"/>
+      <setting name="mapUnderscoreToCamelCase" value="false"/>
+      <setting name="localCacheScope" value="SESSION"/>
+      <setting name="jdbcTypeForNull" value="OTHER"/>
+      <setting name="lazyLoadTriggerMethods" value="equals,clone,hashCode,toString"/>
+    </settings>
+   */
   private Properties settingsAsProperties(XNode context) {
+    //首先检查对应的节点是否存在
     if (context == null) {
+      //不存在就直接new一个属性对象,同时进行返回处理
       return new Properties();
     }
+    //获取配置在settings节点下的所有属性信息
     Properties props = context.getChildrenAsProperties();
     // Check that all settings are known to the configuration class
     MetaClass metaConfig = MetaClass.forClass(Configuration.class, localReflectorFactory);
+    //循环检测对应的属性参数在对应的配置对象中是否有对应的设置属性的方法
     for (Object key : props.keySet()) {
+      //检测配置对象上是否有此key对应的属性设置方法
       if (!metaConfig.hasSetter(String.valueOf(key))) {
+        //没有对应的属性设置方法就相当于可能配置错了相关的属性  进行异常抛出处理  同时指出了是哪个key可能配置的有问题
         throw new BuilderException("The setting " + key + " is not known.  Make sure you spelled it correctly (case sensitive).");
       }
     }
@@ -215,28 +352,6 @@ public class XMLConfigBuilder extends BaseBuilder {
       String type = context.getStringAttribute("type");
       ReflectorFactory factory = (ReflectorFactory) resolveClass(type).newInstance();
       configuration.setReflectorFactory(factory);
-    }
-  }
-
-  private void propertiesElement(XNode context) throws Exception {
-    if (context != null) {
-      Properties defaults = context.getChildrenAsProperties();
-      String resource = context.getStringAttribute("resource");
-      String url = context.getStringAttribute("url");
-      if (resource != null && url != null) {
-        throw new BuilderException("The properties element cannot specify both a URL and a resource based property file reference.  Please specify one or the other.");
-      }
-      if (resource != null) {
-        defaults.putAll(Resources.getResourceAsProperties(resource));
-      } else if (url != null) {
-        defaults.putAll(Resources.getUrlAsProperties(url));
-      }
-      Properties vars = configuration.getVariables();
-      if (vars != null) {
-        defaults.putAll(vars);
-      }
-      parser.setVariables(defaults);
-      configuration.setVariables(defaults);
     }
   }
 
--- mybatis-3.5.1/src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
+++ mybatis-3.5.1/src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -27,11 +27,22 @@ import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 
 /**
  * Builds {@link SqlSession} instances.
+ * 通过使用构建者模式来创建对应的SqlSessionFactory对象  即通过不同配置参数来组建对应的SqlSessionFactory对象
+ *   主要是通过这个方式来提供不同方式进行构建SqlSessionFactory对象的形式
+ *   其中最为重要的一个构建方法是build(Configuration config)  它根据对应的配置信息对象来创建对应的SqlSessionFactory对象
+ *   ----->因此通过这种形式可以发现创建SqlSessionFactory对象的主要形式为
+ *         1.通过对应的字符输入流来引入相应的配置进行并进行解析处理 最终构建对应的Configuration对象    字符流对象  Reader  --->  XMLConfigBuilder --->  parser.parse()  --->  Configuration  --->  SqlSessionFactory
+ *         2.通过对应的字节输入流来引入相应的配置进行并进行解析处理 最终构建对应的Configuration对象    字节流对象  InputStream  --->  XMLConfigBuilder --->  parser.parse()  --->  Configuration  --->  SqlSessionFactory
+ *         3.自己直接new Configuration对象来进行创建并调用构建方法   这样需要自己来创建并初始化Configuration类对象
+ *   需要明确的问题是  Configuration类对象在运行期间只会保持一份  因此 对应的创建对应的SqlSessionFactory对象也会保持一个对象
  *
- * @author Clinton Begin
+ *   通过分析两种流对象的处理可以发现   最终创建配置信息类对象的操作交给了对应的xml解析器对象进行处理  即通过创建了xml配置参数来具体解析并配置对应的配置信息类对象  进而完成根据配置的属性来装配配置信息类对象的处理
  */
 public class SqlSessionFactoryBuilder {
 
+  /**
+   * 总体上通过提供配置信息字符流对象来构建DefaultSqlSessionFactory对象的操作处理
+   */
   public SqlSessionFactory build(Reader reader) {
     return build(reader, null, null);
   }
@@ -46,13 +57,17 @@ public class SqlSessionFactoryBuilder {
 
   public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
     try {
+      //根据对应的字符流对象创建对应的xml解析构建器对象
       XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+      //解析提供的xml配置信息并构建出对应的配置信息类对象，进而创建对应的SqlSessionFactory对象
       return build(parser.parse());
     } catch (Exception e) {
+      //遇到错误抛出对应的构建SqlSessionFactory对象出现错误的异常
       throw ExceptionFactory.wrapException("Error building SqlSession.", e);
     } finally {
       ErrorContext.instance().reset();
       try {
+        //关闭对应的流对象
         reader.close();
       } catch (IOException e) {
         // Intentionally ignore. Prefer previous error.
@@ -60,6 +75,9 @@ public class SqlSessionFactoryBuilder {
     }
   }
 
+  /**
+   * 总体上通过提供配置信息字节流对象来构建DefaultSqlSessionFactory对象的操作处理
+   */
   public SqlSessionFactory build(InputStream inputStream) {
     return build(inputStream, null, null);
   }
@@ -74,13 +92,17 @@ public class SqlSessionFactoryBuilder {
 
   public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
     try {
+      //根据对应的字节流对象创建对应的xml解析构建器对象
       XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
+      //解析提供的xml配置信息并构建出对应的配置信息类对象，进而创建对应的SqlSessionFactory对象
       return build(parser.parse());
     } catch (Exception e) {
+      //遇到错误抛出对应的构建SqlSessionFactory对象出现错误的异常
       throw ExceptionFactory.wrapException("Error building SqlSession.", e);
     } finally {
       ErrorContext.instance().reset();
       try {
+        //关闭对应的流对象
         inputStream.close();
       } catch (IOException e) {
         // Intentionally ignore. Prefer previous error.
@@ -88,7 +110,12 @@ public class SqlSessionFactoryBuilder {
     }
   }
 
+  /**
+   * 本构建方法是最终需要走的构建方法------------>本类中的关键方法
+   *   即上述的两种方式最终都会构建对应的Configuration对象 进而启动创建默认的DefaultSqlSessionFactory对象
+   */
   public SqlSessionFactory build(Configuration config) {
+    //根据提供的配置信息类对象构建默认实现的SqlSessionFactory对象
     return new DefaultSqlSessionFactory(config);
   }
 
